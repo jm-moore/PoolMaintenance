41e2d506837e87875751196a41e927c02122d3e5
jm-moore <jmmoore9358@gmail.com>
2022-09-20 14:11:29 -0500

    initial commit

---
+++ PoolMaintenance/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PoolMaintenance.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
